3f8dbb2055b9f8baa0b25582bd6c3a1a8f6224b7
Unknown <unknown@example.com>
2020-04-09 13:59:48 -0700

    Finished Case 3

--- case3/pc_cword.js
+++ case3/pc_cword.js
@@ -49,6 +49,7 @@
 
 */
 
+//Steps 4 a-f
 var allLetters;
 var currentLetter;
 var wordLetters;
@@ -56,46 +57,184 @@ var acrossClue;
 var downClue;
 var typeDirection = "right";   
 
+//Step 5
 window.onload = init();
 
-function init(){
-   allLetters = document.querySelectorAll("table#crossword span");
+//Step 6
+function init(e) {
+   //Set up initial conditions of the puzzle
+   allLetters = document.querySelectorAll("table#crossword span")
    currentLetter = allLetters[0];
-   var acrossID = ;
-   var downID = ;
-   acrossClue = document.getElementById("acrossID");
-   downClue = document.getElementById("downID");
-   
-}
+   var acrossID = currentLetter.dataset.clueA;
+   var downID = currentLetter.dataset.clueD;
+   acrossClue = document.getElementById(currentLetter.dataset.clueA);
+   downClue = document.getElementById(currentLetter.dataset.clueD);
 
-function formatPuzzle(puzzleLetter){
-   currentLetter = puzzleLetter;
+   //Step 8a
+   formatPuzzle(currentLetter);
+   //Step 8b (Select a puzzle cell using a mouse)
    for(var i = 0; i < allLetters.length; i++){
-      acrossClue.style.backgroundColor = "";
-      downClue.style.backgroundColor = "";
+      //Step 8b i
+       allLetters[i].style.cursor = "pointer";
+       //Step 8b ii
+       allLetters[i].onmousedown = function(e){
+           formatPuzzle(e.target);
+       }
    }
-   acrossClue[i].style.color = "";
-   downClue[i].style.color = "";
-   if(currentLetter.dataset.clueA !== "undefined"){
-      acrossClue = document.getElementById("currentLetter.dataset.clueA");
+
+   //Step 10
+   document.addEventListener("keydown", selectLetter);
+
+   //Step 12a (Change typing direction)
+   var typeImage = document.getElementById("directionImg");
+
+   //Step 12b
+   typeImage.style.cursor = "pointer";
+
+   //Step 12c
+   typeImage.onclick = switchTypeDirection;
+
+   //Step 13
+   document.getElementById("showErrors").onclick = function(){
+      //Steps 13a (Run show errors)
+      for(var i = 0; i < allLetters.length; i++){
+         if(allLetters[i].textContent !== allLetters.dataset.letter.value){
+            allLetters[i].style.color = "red";
+         }
+      }
+      //Steps 13b (Remove the hints after 3 seconds)
+      setTimeout(function(){}, 3000);
+   }
+   
+   //Step 14 (Shows Solution)
+   document.addEventListener("click", function(){
+       for(var i = 0; i < allLetters.length; i++){
+           allLetters.textContent = allLetters.dataset.letter.value;
+       }
+   });
+}
+
+//Step 7
+function formatPuzzle(puzzleLetter){
+
+   //Step 7a
+   currentLetter = puzzleLetter
+
+   //Step 7b
+   for(var i = 0; i < allLetters.length; i++){
+      allLetters[i].style.backgroundColor = "";
+   }
+   //Step 7c
+   acrossClue.style.color = "";
+   downClue.style.color = "";
+
+   //Step 7d
+   if(currentLetter.dataset.clueA !== undefined){
+
+      //Step 7d i
+      acrossClue = document.getElementById("currentLetter.dataset.clueA")
+
+      //Step 7d ii
       acrossClue.style.color = "blue";
-      wordLetters = "[data-clue-A = " +  + "]";
-      wordLetters.style.backgroundColor = "rgb(231, 231, 255)";
+
+      //Step 7d iii
+      wordLetters = "[data-clue-A = " + data-clue-a.value + "]";
+
+      //Step 7d iv
+      wordLetters[i].style.backgroundColor = "rgb(231, 231, 255)";
    }
-   else if(currentLetter.dataset.clueA !== "undefined"){
-      acrossClue = document.getElementById("currentLetter.dataset.clueA");
-      acrossClue.style.color = "blue";
-      wordLetters = "[data-clue-A = " +  + "]";
-      wordLetters.style.backgroundColor = "rgb(231, 231, 255)";
+
+   //Step 7e
+   if(currentLetter.dataset.clueD !== undefined){
+      downClue = document.getElementById("currentLetter.dataset.clueD")
+      downClue.style.color = "red";
+      wordLetters = "[data-clue-D = " + data-clue-d.value + "]";
+      wordLetters[i].style.backgroundColor = "rgb(255, 231, 231)";
    }
+
+   //Step 7f
    if(typeDirection === "right"){
-      currentLetter.style.backgroundColor = "rgb(191, 191, 255)";
+      currentLetter.style.color = "rgb(191, 191, 255)";
    }
-   else(typeDirection !== "right"){
-      currentLetter.style.backgroundColor = "rgb(255, 191, 191)";
+   else{
+      currentLetter.style.color = "rgb(255, 191, 291)";
    }
 }
 
+//Step 9
+function selectLetter(e) {
+
+   //Step 9a
+   var leftLetter = document.getElementById(currentLetter.dataset.left);
+   var upLetter = document.getElementById(currentLetter.dataset.up);
+   var rightLetter = document.getElementById(currentLetter.dataset.right);
+   var downLetter = document.getElementById(currentLetter.dataset.down);
+
+   //Step 9b
+   var userKey = e.keyCode;
+
+   //Step 9c i
+   if(userKey === 37){
+      formatPuzzle(leftLetter);
+   }
+  
+
+ //Step 9c ii
+   if(userKey === 38){
+        formatPuzzle(upLetter);
+   }
+
+   //Step 9c iii
+   else if(userKey === 39 || 9){
+        formatPuzzle(rightLetter);
+   }
+
+   //Step 9c iv
+   else if(userKey === 40 || 13){
+        formatPuzzle(downLetter);
+   }
+
+   //Step 9c v
+   else if(userKey === 8 || userKey === 46){
+       currentLetter.textContent = "";
+   }
+
+   //Step 9c vi
+   else if(userKey === 32){
+      switchTypeDirection();
+   }
+
+   //Step 9c vii
+   else if(userKey >= 65 && userKey <= 90){
+      currentLetter.textContent = getChar(userKey);
+      if(typeDirection === "right"){
+         formatPuzzle(rightLetter);
+      }
+      else{
+         formatPuzzle(downLetter);
+      }
+   }
+
+   //Step 9d
+   preventDefault();
+}
+
+//Step 11
+function switchTypeDirection(){
+   //Step 11a
+   var typeImage = document.getElementById("directionImg");
+   //Step 11b and c
+   if(typeDirection === "right"){
+	 typeDirection = "down";
+       typeImage.style.src = "pc_right.png";
+       currentLetter[i].style.backgroundColor = "rgb(255, 191, 191)";
+   }
+   else{
+       typeDirection = "right";
+       typeImage.style.src = "pc_down.png";
+       currentLetter.style.backgroundColor = "rgb(191, 191, 255)"; 
+   }
+}
 /*====================================================*/
 
 function getChar(keyNum) {
